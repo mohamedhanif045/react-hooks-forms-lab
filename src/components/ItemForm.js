fe3a83f514ce9baa4079a89d9666c29e6e016ec9
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-07-14 20:31:06 +0300

    react

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,32 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, { useState } from "react";
+import ItemForm from "./ItemForm";
+import Filter from "./Filter";
+import Item from "./Item";
 
-function ItemForm(props) {
+function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-
-      <label>
-        Category:
-        <select name="category">
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
-    </form>
+    <div className="ShoppingList">
+      <ItemForm />
+      <Filter onCategoryChange={handleCategoryChange} />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default ItemForm;
+export default ShoppingList;
